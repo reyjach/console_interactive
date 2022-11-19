ce3f37d76be89f4e7f66ebcd42ead460e42f9ef9
Andres Martinez <reymuerto14@hotmail.com>
2022-11-18 19:58:32 -0500

    se modifico el menu para que lea una promesa y o valide que se salga

--- app.js
+++ app.js
@@ -6,7 +6,19 @@ const { mostarMenu, pausa } = require('./helpers/mensajes');
 const main = async() =>{
     console.log('hola mundo');
 
-    mostarMenu();
+    let opt = '';
+
+    
+
+    do {
+        
+        opt = await mostarMenu();
+
+        console.log({ opt });
+
+        if(opt !== '0') await pausa();
+
+    } while(opt !== '0');
 
     //pausa();
 }
